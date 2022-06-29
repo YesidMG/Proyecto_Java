0a15045a9ee50fe30176f24f5004c4cb5897eb9b
R4302 <yesid.martinez04@uptc.edu.co>
2022-06-29 10:00:01 -0500

    primer comit

---
+++ src/ejercicio_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ejercicio_1 {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner (System.in);
+		String a = "carro";
+		String b = "moto";
+		String c = a + b;
+		String data;
+	    data=a+b;
+	    char r='ñ';
+	    
+		System.out.println(data);	
+		System.out.println(a+" "+b);
+		System.out.println(r + a);	
+	}
+
+}
